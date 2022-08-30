85f666f729c8e6eb153291b29b896cb13bfc33c9
Tamura77 <tnbddd77@gmail.com>
2022-08-30 17:55:32 +0800

    Changed title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aspire race around campus
         </a>
       </header>
     </div>
