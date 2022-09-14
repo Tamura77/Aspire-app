5bf62a1b2808b225fcc1ec1c252e13eb08e246c1
L-Lamont <74656778+L-Lamont@users.noreply.github.com>
2022-09-14 15:52:02 +0800

    Added basic styling to navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -4,29 +4,27 @@ import { BrowserRouter as Router, Link } from "react-router-dom";
 // Icon imports
 import { TbDeviceGamepad2, TbMap2 } from "react-icons/tb";
 import { RiLinksLine } from "react-icons/ri";
+import "./navbar.css";
 
 // Navbar imports
-import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar"
 
 // Navbar Component
 function AspireNavbar() {
     return (
-        <Navbar expand="sm" bg="dark" variant="dark">
-            <Container>
-                <Nav>
-                    <Nav.Link href="/PageNotFound">
-                        <TbDeviceGamepad2 />
-                    </Nav.Link>
-                    <Nav.Link href="/Map">
-                        <TbMap2 />
-                    </Nav.Link>
-                    <Nav.Link href="/PageNotFound">
-                        <RiLinksLine />
-                    </Nav.Link>
-                </Nav>
-            </Container>
+        <Navbar className="fixed-bottom navbarDefault" expand="sm" bg="light" variant="light">
+            <Nav className="m-auto">
+                <Nav.Link href="/PageNotFound">
+                    <TbDeviceGamepad2 className="iconDefault" />
+                </Nav.Link>
+                <Nav.Link href="/Map">
+                    <TbMap2 className="iconDefault"/>
+                </Nav.Link>
+                <Nav.Link href="/PageNotFound">
+                    <RiLinksLine className="iconDefault"/>
+                </Nav.Link>
+            </Nav>
         </Navbar>
     );
 }
