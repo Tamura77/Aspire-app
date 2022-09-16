9159aaf9beab20b2240185f6ec565798fcc798f3
Charles Ong <charlesong173@gmail.com>
2022-09-16 18:33:13 +0800

    added 10 tasks to racemode

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -26,34 +26,77 @@ import {
     
     const markersDB = [
       {
-        name: "Building 1",
-        coordinates: [-58.3816, -34.6037],
+        name: "Arts Building",
+        coordinates: [-20, 78.5],
         online: true,
-        info: "tall building",
+        task: "How many peacocks and peahens live on campus?",
+        colour: "#FF0000",
         number: 0,
-        colour: "#008000"
       },
       {
-        name: "Building 6",
-        coordinates: [144.963058, -37.813629],
+        name: "University Club of Western Australia",
+        coordinates: [10, 78.5],
         online: true,
-        info: "small building",
+        task: "Which Academy-Award winning artist (and UWA graduate) created the 'Hours to Sunset' sundial on the wall of University Club? What did they win their award for? (2 points)",
         colour: "#FF0000"
       },
       {
-        name: "Building 4",
-        coordinates: [-122.419418,37.774929],
+        name: "Reid Library",
+        coordinates: [-62,71],
         online: true,
-        info: "tall building",
+        task: "How many floors does the Reid Library have?",
         colour: "#FF0000"
       },
       {
-        name: "Building 2",
-        coordinates: [0,0],
+        name: "Guild Village",
+        coordinates: [8,10],
         online: true,
-        info: "tall building",
+        task: "Name two of the shops located in the guild village.",
         colour: "#FF0000"
       },
+      {
+        name: "Bilya Marlee",
+        coordinates: [48,-68],
+        online: true,
+        task: "Who created the artwork, “Danjoo Kaartdijin”, located in the front entrance of the Bilya Marlee building? What was the artwork a metaphor for? (2 points)",
+        colour: "#FF0000"
+      },
+      {
+        name: "Barry J Marshall Library",
+        coordinates: [-30,-65],
+        online: true,
+        task: "The Science Library is named after Barry J Marshall. What year did he receive his Nobel Prize? What was it for? (2 points)",
+        colour: "#FF0000"
+      },
+      {
+        name: "Bayliss Building",
+        coordinates: [-32,-35],
+        online: true,
+        task: "Take a photo of the ground floor tiling in the central atrium of the Bayliss building. Describe the 'Penrose' pattern. (Hint - it uses symmetry)",
+        colour: "#FF0000"
+      },
+      {
+        name: "Irwin Street Building",
+        coordinates: [-100,50],
+        online: true,
+        task: "What is special about the Irwin Street Building (the white building next to James Oval)?",
+        colour: "#FF0000"
+      },
+      {
+        name: "EZONE",
+        coordinates: [-130,50],
+        online: true,
+        task: "Each room in the new Ezone building is named using Indigenous words. What is the word for the EMS Student Office?",
+        colour: "#FF0000"
+      },
+      {
+        name: "Physics building",
+        coordinates: [-102,77],
+        online: true,
+        task: "What city is the statue in the foyer of the Physics building from?",
+        colour: "#FF0000"
+      },
+      
       
     ];
     
