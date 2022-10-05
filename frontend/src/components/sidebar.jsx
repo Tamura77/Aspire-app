2409f25727048c33b2f52514713777bcc9f0992c
Tamura77 <tnbddd77@gmail.com>
2022-10-05 21:18:46 +0800

    added dynamic markers to locations

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -1,51 +1,4 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
-import { BsListTask } from "react-icons/bs";
-import { BsPinMap } from "react-icons/bs";
-import { BsSpeedometer2 } from "react-icons/bs";
-import './sidebar.css'
-
-const Pages = [
-    {
-    name: "Tasks",
-    icon: <BsListTask/>,
-    link: "/tasks"
-},
-{
-    name: "Locations",
-    icon: <BsPinMap/>,
-    link: "/locations"
-},
-{
-    name: "Races",
-    icon: <BsSpeedometer2/>,
-    link: "/races"
-}
-]
-
-function Sidebar(){
-    return(
-        <div className="Sidebar">
-            <ul className="SidebarList">
-                {Pages.map((val,key) =>{
-                    return (
-                        <li key = {key} 
-                        className="row"
-                        onClick={() => {
-                            window.location.pathname = val.link;
-                        }}>
-                            <div> {val.icon}</div> <div>{val.name}</div>
-                </li>
-                    );
-                })}
-                
-            </ul>
-        </div>
-    )
-}
-
-export default Sidebar
-=======
 import { BrowserRouter as Router, Link } from "react-router-dom";
 
 import "./sidebar.css";
@@ -84,4 +37,3 @@ function sidebar(){
     );
 }
 export default sidebar
->>>>>>> 20302be54a1b442c8645cadc11b6926bca815778
--- frontend/src/pages/AdminPlaces.jsx
+++ frontend/src/pages/AdminPlaces.jsx
@@ -3,11 +3,38 @@ import React, { useState } from "react";
 //Components
 import Sidebar from "../components/sidebar";
 
+//icons
+import {BsFillCircleFill} from "react-icons/bs"
 //Styling
 import "./styling/AdminSide.css"
 import "../components/sidebar.css"
 
+//map SVG file
+import map from "../assets/images/campusmap.svg";
+
+//Variables
+const state ={
+  xoffset: 0,
+  yoffset: 0,
+  delta: 0,
+  opacity: 0.0,
+};
+
+
 function AdminPlaces () {
+  const [coord, setCoord] = useState([])
+  const handleMouseMove = (event) =>{
+    let coordinates = [event.clientX, event.clientY]
+    setCoord(coordinates)
+  }
+  console.log(coord)
+
+  //store coordinates function
+  const store = event =>{
+     state.xoffset = coord[0] -17;
+     state.yoffset = coord[1] -28;
+     state.opacity = 1;
+  }
   return (
     <>
     <div className="admin-div">
@@ -34,6 +61,11 @@ function AdminPlaces () {
             </div>
             <div className="database-table">
                 <h1>Map</h1>
+                <div id="marker" style={{position: "absolute", left: `${state.xoffset}px`, top: `${state.yoffset}px`, opacity: `${state.opacity}`,}}>
+                <BsFillCircleFill/>
+                </div>
+                <img className="mapsvg" width="550" height="1500" onMouseMove={(event) => handleMouseMove(event)} onClick={store}src={map} alt="campus map">
+                </img>
             </div>
         </div>
     </div>
