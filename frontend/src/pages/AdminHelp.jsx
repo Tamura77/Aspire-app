195019947a4e57c620f7092bf9fd69a6b97fb0d9
L-Lamont <74656778+L-Lamont@users.noreply.github.com>
2022-10-12 11:35:51 +0800

    Added content to the admin help page

--- frontend/src/pages/AdminHelp.jsx
+++ frontend/src/pages/AdminHelp.jsx
@@ -25,116 +25,75 @@ function AdminHelp() {
             <Sidebar/>
             <div className="helpWrappingDiv" style={{width: "100%"}}>
                 <Container>
-                    <h1>THIS NEEDS TO BE ADAPTED TO BE FOR THE ADMIN PAGE</h1>
                     <div className="helpDiv" align="left">
                         <Row>
-                            <Col>
-                                <div className="helpTitle" align="center">Race Mode</div>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col md="9">
-                                <div className="helpText">
-                                    Switch to the race page and enter the race code provided. Then wait for the race to start
-                                </div>
-                            </Col>
-                            <Col md="3">
-                                <TbDeviceGamepad2 className="helpIcon" />
+                            <Col> 
+                                <div className="helpTitle">Tables Page</div>
                             </Col>
                         </Row>
                         <Row>
                             <Col>
                                 <div className="helpText">
-                                    Answer all the questions then submit your answers for marking
+                                    The Tables page gives an overview of all the tasks created,
+                                    all locations created and the tasks in the current race which
+                                    will be shown in the race mode.
                                 </div>
                             </Col>
                         </Row>
                         <Row>
-                            <Col>
-                                <div className="helpSubHeader">
-                                    To answer a question
-                                </div>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col md="9">
-                                <div className="helpText">
-                                    Press on the waypoint and answer in the popup<br/>
-                                </div>
-                            </Col>
-                            <Col md="3">
-                                <MdLocationPin className="helpIcon redIcon" align="center" />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col md="9">
-                                <div className="helpText">
-                                    Once you answer the waypoint will turn orange, indicating its time to get your answer checked
-                                </div>
-                            </Col>
-                            <Col md="3">
-                                <MdLocationPin className="helpIcon orangeIcon" align="center" />
+                            <Col> 
+                                <div className="helpTitle">Tasks Page</div>
                             </Col>
                         </Row>
                         <Row>
                             <Col>
                                 <div className="helpText">
-                                    After answering you will have the option to submit your answers, at this point press the submit button and pass the device to an administrator for marking
-                                </div>
-                            </Col>
-                        </Row>
-                    </div>
-                    <div className="helpDiv" align="left">
-                        <Row>
-                            <Col>
-                                <div className="helpTitle" align="center">
-                                    Information Mode
+                                    Tasks are the content of the race and contain a question and a location
+                                    The tasks page shows the current tasks that have been created on the right
+                                    hand side and allows you to add more using the left hand side.
+                                    To add a task select a location and enter the question then click post
+                                    To modify or delete a task enter the corresponding task ID and then click 
+                                    the delete button or enter a new question or location and click update.
                                 </div>
                             </Col>
                         </Row>
                         <Row>
-                            <Col md="9">
-                                <div className="helpText">
-                                    Switch to the information page using the navbar
-                                </div>
-                            </Col>
-                            <Col md="3">
-                                <TbMap2 className="helpIcon" />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col md="9">
-                                <div className="helpText">
-                                    Press on a waypoint to show additional information about that location
-                                </div>
-                            </Col>
-                            <Col md="3">
-                                <MdLocationPin className="helpIcon" />
-                            </Col>
-                        </Row>
-                    </div>
-                    <div className="helpDiv" align="left">
-                        <Row>
-                            <Col>
-                                <div className="helpTitle" align="center">
-                                    Useful Links 
-                                </div>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col md="9">
-                                <div className="helpText">
-                                    Switch to the information page using the navbar
-                                </div>
-                            </Col>
-                            <Col md="3">
-                                <RiLinksLine className="helpIcon" />
+                            <Col> 
+                                <div className="helpTitle">Race Page</div>
                             </Col>
                         </Row>
                         <Row>
                             <Col>
                                 <div className="helpText">
-                                    Click on the link of the page you want to visit 
+                                    A race is a collection of tasks in a specific order the race is configured in
+                                    the Race page. The race page shows you the current race on the right hand side and 
+                                    allows you to modify the race using the left hand side. To add to the race select 
+                                    a task and the race name then click add. To delete a task from a race enter its Race
+                                    ID and press delete. To change one of the tasks enter its Race ID then select a new 
+                                    task and press update. The order of the tasks is important as it is displayed to the 
+                                    users and tells them the order to do the tasks in. The order of the tasks can be 
+                                    considered a loop and the starting and finishing positions can be altered using 2 
+                                    letters at the end of the race code the users enter before starting a race. ab will 
+                                    start from the 1st task and end at the last task, cd will start at the 2nd task and
+                                    end at the 1st and so on. This allows one set of tasks to create many races.
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col> 
+                                <div className="helpTitle">Locations Page</div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <div className="helpText">
+                                    The locations page allows you to manage the places where tasks can be set and the places 
+                                    displayed in the information mode. To add a new location press somewhere on the map enter 
+                                    a description, a place name and then click post. To access the lower half of the map click 
+                                    down button and then click on a location, to return to the top half click the up button. To
+                                    delte a location enter the Place ID (available on the tables page) then click delete. To update 
+                                    a location enter the Place ID and then edit any of the place name, description or coordinate 
+                                    fields and then click update.
                                 </div>
                             </Col>
                         </Row>
