6c0cf9370459827a58ebc66ded9a79a8b134b213
Tintin9866 <tintinthemlgpug@gmail.com>
2022-10-15 12:56:08 +0800

    Help page appears on first page load

--- frontend/src/components/helpButton.jsx
+++ frontend/src/components/helpButton.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import Modal from 'react-bootstrap/Modal';
 import Help from "./helpInfo";
@@ -18,6 +18,20 @@ function HelpButton() {
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    function checkFirst() {
+        if (document.cookie.split(';').some((item) => item.trim().startsWith('first-open='))) {
+            return;
+          } else {
+            setShow(true);
+            document.cookie = "first-open=no"
+          }
+    }
+
+    useEffect(() => {
+        checkFirst();
+      }, [])
+
     return (
         <div className="fixed-top" align="right">
             <Button className="helpButton" align="right" onClick={handleShow}>
